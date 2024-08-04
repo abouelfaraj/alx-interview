b6b0796225abc21318e8f0894c7e062a8a9fe156
root <aef.ayoub@gmail.com>
2024-08-04 15:39:15 +0000

    cmt queens function

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-        
+
     try:
         n = int(sys.argv[1])
     except ValueError:
