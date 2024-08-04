03aecde734479bd464b111d5784cfff4e6b0b8f5
root <aef.ayoub@gmail.com>
2024-08-04 15:19:42 +0000

    cmt task nquens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,4 +1,4 @@
-#!/usrin/python3
+#!/usr/bin/python3
 '''N Queens Backtrack'''
 
 import sys
@@ -8,20 +8,21 @@ if __name__ == '__main__':
         print("Usage: nqueens N")
         sys.exit(1)
         
-        try:
-            n = int(sys.argv[1])
-        except ValueError:
-            print('N must be at least 4')
-            exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be at least 4')
+        exit(1)
 
     if n < 4:
         print('N must be at least 4')
         exit(1)
 
     solutions = []
-    p_queens = []
+    placed_queens = []
     stop = False
     r = 0
+    c = 0
 
     while r < n:
         goback = False
